6c04db737b4c171eb3c0f8c2f392977bd005020e
GolubtsevaMI <maria.golubtseva.extern@elinvar.de>
2023-03-13 13:35:19 +0300

    Bug fixes: name of the constant, removed extra spaces, moved methods to separate classes

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,65 +1,14 @@
 import java.util.Scanner;
 
 public class Calculator {
-    static Scanner scanner = new Scanner(System.in);
-
+    static Scanner SCANNER = new Scanner(System.in);
     public static void main(String[] args) {
-        double num1 = getDouble();
-        double num2 = getDouble();
-        char operation = getOperation();
-        double result = calc(num1, num2, operation);
+        double num1 = CalculatorInputUtils.getDouble();
+        double num2 = CalculatorInputUtils.getDouble();
+        char operation = CalculatorInputUtils.getOperation();
+        double result = Calculation.calculate(num1, num2, operation);
         System.out.println("Result of operation: " + result);
     }
-
-
-    public static double getDouble() {
-        System.out.println("Enter number: ");
-        double num;
-        if (scanner.hasNextDouble()) {
-            num = scanner.nextDouble();
-        } else {
-            System.out.println("Wrong number, try again.");
-            scanner.next();
-            num = getDouble();
-        }
-        return num;
-    }
-
-
-    public static char getOperation() {
-        System.out.println("Choose operation + - * / ");
-        char operation = 0;
-        if (scanner.hasNext()) {
-            operation = scanner.next().charAt(0);
-        }
-
-        return operation;
-    }
-
-
-    public static double calc(double num1, double num2, char operation) {
-        double result = 0;
-        switch (operation) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                result = num1 / num2;
-                break;
-            default:
-                System.out.println("Wrong operation, try again.");
-                result = calc(num1, num2, getOperation());
-                return result;
-        }
-        return result;
-    }
-
 }
 
 
